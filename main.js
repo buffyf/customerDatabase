6e5b4702f1c757d405ae8c983f8f6d28e82cbf4c
Buffy Feinstein <buffyfeinstein@gmail.com>
2017-08-01 16:16:15 -0500

    css needs to be tweaked

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ for (let i = 0; i < customers.results.length; i++) {
         <p class="fullName">${customers.results[i].name.first}  ${customers.results[i].name.last}</p>
         <p class="email">${customers.results[i].email}</p>
         <p class="address">${customers.results[i].location.street}</p>
-        <p class="address">${customers.results[i].location.city}  ${customers.results[i].location.state} ${customers.results[i].location.postcode}</p>
-        <p class="address">${customers.results[i].phone}</p>
+        <p class="address">${customers.results[i].location.city},  ${customers.results[i].location.state} ${customers.results[i].location.postcode}</p>
+        <p class="address">${customers.results[i].phone} </p>
         <p class="ssn">${customers.results[i].id.value}</p>
       </div>
 
